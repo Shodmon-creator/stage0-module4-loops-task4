f4129a70f67b685902f80b721c24538c61509564
Shodmon-creator <mamayusupovshodmon2003@gmail.com>
2022-11-05 20:44:30 +0500

    4 tasks has been done

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -3,5 +3,30 @@ package school.mjc.stage0.loops.task4;
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
 
+        if (toBreakWith>numberToGoUntil){
+            System.out.println("iterating till the end");
+            for (int i = 1; i <=numberToGoUntil ; i++) {
+                System.out.println(i);
+            }
+        }else {
+            for (int j = 1; j <numberToGoUntil; j++) {
+                System.out.println(j);
+                if (j==toBreakWith)
+                    break;
+            }
+        }
+
+
+//        for (int i = 1; i <= numberToGoUntil; i++) {
+//            if (toBreakWith < numberToGoUntil) {
+//                System.out.println(i);
+//                if (i == toBreakWith)
+//                    break;
+//            } else {
+//                System.out.println("iterating till the end");
+//                System.out.println(i);
+//            }
+//        }
+
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,6 +2,33 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        int countedSum = 0;
+        int skippedSum = 0;
+
+        for (int i = 0; i < Math.abs(lastInRow); i++) {
+            if (i!=numberToSkip){
+                countedSum+=i;
+            }else {
+                skippedSum+=i;
+            }
+
+
+            if (countedSum > lastInRow) {
+                System.out.println("number to skip is bugger then the last");
+                break;
+            } else if (lastInRow < 0) {
+                System.out.println("last number in row is negative");
+                break;
+            } else {
+                System.out.println("skipped sum is " + skippedSum);
+                System.out.println("counted sum is " + countedSum);
+                break;
+            }
+        }
+
+
+
+
 
     }
 }
